916577f494e01059ff5a25ce74bec93b87bb350a
Emily Dolson <emilyldolson@gmail.com>
2018-07-08 05:17:49 +0000

    structural method works now

--- motion_capture.py
+++ motion_capture.py
@@ -10,36 +10,44 @@ from skimage.measure import compare_ssim as ssim
 
 prior_image = None
 
-THRESHOLD = 10000
+THRESHOLD = .92
 
 def get_time_string():
     return datetime.datetime.now().isoformat()
 
 def detect_motion(camera):
     global prior_image
-    stream = io.BytesIO()
-    camera.capture(stream, format='jpeg', use_video_port=True)
-    stream.seek(0)
+    #stream = io.BytesIO()
+    #camera.capture(stream, format="rgb", use_video_port=True)
+    #stream.seek(0)
+    #print(camera.resolution[0])
     if prior_image is None:
-        prior_image = np.empty((camera.resolution[0], camera.resolution[1], 3), dtype=np.uint8)
-        camera.capture(prior_image, 'rgb')
+        prior_image = np.empty((camera.resolution[0] * camera.resolution[1] * 3), dtype=np.uint8)
+	prior_image.setflags(write=True)
+        camera.capture(prior_image, format='rgb', use_video_port=True)
         return False
     else:
-        current_image = np.empty((camera.resolution[0], camera.resolution[1], 3), dtype=np.uint8)
-        camera.capture(current_image, 'rgb')
+        current_image = np.empty((camera.resolution[0] * camera.resolution[1] * 3), dtype=np.uint8)
+        camera.capture(current_image, format='rgb', use_video_port=True)
 
         # Compare current_image to prior_image to detect motion. This is
         # left as an exercise for the reader!
-        result = ssim(prior_image, current_image, data_range=prior_image.max() - prior_image.min()) > THRESHOLD
+        result = ssim(prior_image, current_image, data_range=prior_image.max() - prior_image.min())
+	print(result)
         
         # Once motion detection is done, make the prior image the current
         prior_image = current_image
-        return result
+        return result < THRESHOLD
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
-    stream = picamera.PiCameraCircularIO(camera, seconds=10)
+    stream = picamera.PiCameraCircularIO(camera, seconds=20)
     camera.start_recording(stream, format='h264')
+
+    # Let camera get into focus
+    for i in range(3):
+	detect_motion(camera)
+
     try:
         while True:
             camera.wait_recording(1)
@@ -49,11 +57,11 @@ with picamera.PiCamera() as camera:
                 # record the frames "after" motion
                 # camera.split_recording('after.h264')
                 # Write the 10 seconds "before" motion to disk as well
-                seconds_to_include = 5
+                seconds_to_include = 10
                 # Wait until motion is no longer detected, then split
                 # recording back to the in-memory circular buffer
                 while detect_motion(camera):
-                    camera.wait_recording(1)
+                    camera.wait_recording(2)
                     seconds_to_include += 1
                 stream.copy_to("/home/pi/rpi-sync/videos/" + get_time_string() + ".h264", seconds=seconds_to_include)
                 stream.clear()                
